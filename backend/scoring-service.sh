0fb638afacab5c6b644efd2666ee4e64905cebd7
starnold <starnold@redhat.com>
2023-08-30 15:15:41 +0100

    changing the image to uths new image

--- backend/scoring-service.sh
+++ backend/scoring-service.sh
@@ -1,4 +1,4 @@
 oc project battleships-scoring 2> /dev/null || oc new-project battleships-scoring
 
-oc new-app quay.io/redhatdemo/2021-scoring-service --name="scoring-service" --as-deployment-config=true --env="QUARKUS_INFINISPAN_CLIENT_SERVER_LIST=datagrid.datagrid.svc.cluster.local:11222" --env="QUARKUS_HTTP_CORS=true"
+oc new-app quay.io/uksummit2023/scoringservice:latest --name="scoring-service"  --env="QUARKUS_INFINISPAN_CLIENT_SERVER_LIST=datagrid.datagrid.svc.cluster.local:11222" --env="QUARKUS_HTTP_CORS=true"
 oc apply -f backend/scoring-route-secure.yaml
